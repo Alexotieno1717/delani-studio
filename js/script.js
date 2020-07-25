d23179ecedf254fc153b2a632e3e276e030954e9
Alexotieno1717 <alexotieno900@gmail.com>
2020-07-25 13:41:12 +0300

    Added jQuery to what we do

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    //Design on click text Appear
+    $('#design').click(function(){
+        $('#design').slideToggle();
+        $('#design-text').slideToggle();
+    });
+    $('#design-text').click(function(){
+        $('#design').slideToggle();
+        $('#design-text').slideToggle();
+    });
+
+    //Developement on click text appear
+    $('#developement').click(function(){
+        $('#developement').slideToggle();
+        $('#developement-text').slideToggle();
+    });
+    $('#developement-text').click(function(){
+        $('#developement').slideToggle();
+        $('#developement-text').slideToggle();
+    })
+
+    //Product on click text appear
+    $('#product').click(function(){
+        $('#product').slideToggle();
+        $('#product-text').slideToggle();
+    });
+    $('#product-text').click(function(){
+        $('#product').slideToggle();
+        $('#product-text').slideToggle();
+    });
+});
